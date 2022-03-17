1a0ece7c3271ba7bf44784abd6f6a844da57df55
Marko K <77128380+Marko0513@users.noreply.github.com>
2022-03-17 17:23:04 +0100

    hello_cargo and check the variables

---
+++ hello_cargo/target/debug/deps/hello_cargo-e750a9b6cab672ac.d
@@ -0,0 +1,5 @@
+E:\Ongoing project\hello_world\hello_cargo\target\debug\deps\hello_cargo-e750a9b6cab672ac.rmeta: src\main.rs
+
+E:\Ongoing project\hello_world\hello_cargo\target\debug\deps\hello_cargo-e750a9b6cab672ac.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\Ongoing project\hello_world\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\Ongoing project\hello_world\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
