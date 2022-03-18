1598e5c628f804ed36084f1d990133ee265efd6d
Marko0513 <eternity.authority@gmail.com>
2022-03-18 08:48:18 +0100

    finish the function and variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is {}", x);
@@ -8,8 +10,61 @@ fn main() {
 
     println!("seconds per day: {}", TIME_OF_DAY);
 
+    let heart_eyed_cat = '😻';
+
+    println!("heart eyed cat {}", heart_eyed_cat);
+
+    let a = [3; 5];
+    println!(" {}", a[4]);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+    another_function();
     // error code, different type of result
 
     // let mut spaces = "    ";
     // spaces = spaces.len();
+}
+
+fn another_function() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    let z = five();
+    println!("It's call of another function and show the y and z: {} {}", y, z);
+
+    //check the condition
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("make the condition and check the if case {}", number);
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn five() -> i32 {
+    5 //in that case, you should not use the ;(return the value)
 }
